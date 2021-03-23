33027c41ec6e5a2ae1808eda65f166420adc0e36
domo <18298357641@163.com>
2021-03-23 18:37:22 +0800

    调整学习率0.00002-->0.0002

--- options/base_options.py
+++ options/base_options.py
@@ -22,7 +22,7 @@ class BaseOptions():
         # basic parameters
         parser.add_argument('--dataroot', default='/home/tt/2TB/Datasets/CelebA/processed/img_align_celeba_processed/Black2Blond',
                             help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
-        parser.add_argument('--name', type=str, default='Black2Blond',
+        parser.add_argument('--name', type=str, default='Black2Blond-20210323',
                             help='name of the experiment. It decides where to store samples and models')
         parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
